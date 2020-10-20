27631fb9ca86060321c023a8d563e333207503e3
alisajadih <alisajadih@gmail.com>
2020-10-20 14:59:21 +0330

    fonts & signup page

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,17 @@
 <script>
-  import Button from "./Button.svelte";
+  import Form from "./Signup/Form.svelte";
+  import Header from "./Signup/Header.svelte";
+  import Signup from "./Signup/index.svelte";
 </script>
 
-<Button />
+<style>
+  .app {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<div class="app">
+  <Signup />
+</div>
+<!-- <Form /> -->
