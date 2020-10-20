315eaf45bc5867ee332e84a60d50ea0138b302ce
Mohamadhassan Ebrahimi <emohamadhassan@gmail.com>
2020-10-20 17:00:30 +0330

    Fonts added

--- src/Signup/index.svelte
+++ src/Signup/index.svelte
@@ -1,13 +1,13 @@
 <script>
-import Header from './Header.svelte'
-import Form from './Form.svelte'
+    import Header from './Header.svelte'
+    import Form from './Form.svelte'
 </script>
 <style>
-.signup-container{
-    position: relative;
-}
+    .signup-container {
+        position: relative;
+    }
 </style>
-<div class="signup-container">
-<Header/>
-<Form/>
+<div class='signup-container'>
+  <Header/>
+  <Form/>
 </div>
--- src/success/Page.svelte
+++ src/success/Page.svelte
@@ -5,21 +5,27 @@
     .flex {
         display: flex;
     }
+
     .flex-col {
         flex-direction: column;
     }
+
     .flex-row {
         flex-direction: row;
     }
+
     .justify-center {
         justify-content: center;
     }
+
     .items-center {
         align-items: center;
     }
+
     .justify-around {
         justify-content: space-around;
     }
+
     #upper {
         background: #fef3e6;
         height: 326px;
@@ -27,6 +33,7 @@
         border-bottom-left-radius: 30px;
         border-bottom-right-radius: 30px;
     }
+
     #logo {
         width: 100px;
         height: 100px;
@@ -35,18 +42,20 @@
         background: rgba(0, 65, 109, 0.05);
         margin-top: 50px;
     }
+
     #success-message {
         font-size: 21px;
         color: #f9a44a;
         margin-top: 30px;
+        font-weight: 700;
     }
 </style>
 <div class='flex flex-col items-center'>
-    <div id='upper' class='flex flex-col items-center'>
-        <div id='logo' class='flex flex-col items-center justify-center'>
-            Logo
-        </div>
-        <p id='success-message'>شماره موبایل شما با موفقیت ثبت شد.</p>
+  <div id='upper' class='flex flex-col items-center'>
+    <div id='logo' class='flex flex-col items-center justify-center'>
+      Logo
     </div>
-    <GetApp/>
+    <p id='success-message'>شماره موبایل شما با موفقیت ثبت شد.</p>
+  </div>
+  <GetApp/>
 </div>
